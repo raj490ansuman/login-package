8ffeb34092ae2816e9c9a761738f148b7cd0f6a5
rajansuman490 <anshumanraj490@gmail.com>
2024-10-24 14:42:44 +0900

    app.tsx update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import LoginForm from "./components/LoginForm";
 import Register from "./components/Register";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        {/* Default route points to login */}
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   );
 };
 
